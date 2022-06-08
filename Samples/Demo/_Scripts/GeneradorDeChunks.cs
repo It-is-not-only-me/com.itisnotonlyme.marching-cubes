0c094fb7e575a46f469a655b7bfa9684a0dbffdf
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-06-07 22:37:22 -0300

    Eliminando lo que ya no es necesario

--- Samples/Demo/_Scripts/GeneradorDeChunks.cs
+++ Samples/Demo/_Scripts/GeneradorDeChunks.cs
@@ -9,16 +9,28 @@ public class GeneradorDeChunks : MonoBehaviour
     [SerializeField] private int _radio;
     [SerializeField] private Vector3Int _dimensionDeChunk;
 
-    private void Awake()
+    private IEnumerator Start()
     {
+        int contador = 0;
         for (int i = 0; i < _radio; i++)
+        {
             for (int k = 0; k < _radio; k++)
             {
                 GameObject chunk = Instantiate(_prefab, transform);
                 GeneradorDatosPerlin generador = chunk.GetComponent<GeneradorDatosPerlin>();
 
-                Vector3 posicion = transform.position + Vector3.right * (i - 1/2) * _dimensionDeChunk.x + Vector3.forward * (k - 1 / 2) * _dimensionDeChunk.z;
+                Vector3 posicion = transform.position + Vector3.right * (i - 1 / 2) * _dimensionDeChunk.x + Vector3.forward * (k - 1 / 2) * _dimensionDeChunk.z;
                 generador.Inicializar(posicion, _dimensionDeChunk);
+
+                if (contador >= 10)
+                {
+                    contador = 0;
+                    yield return null;
+                }
+
+                contador++;
             }
+
+        }
     }
 }
